9fceade246666c039d916d7a8b39e393344e3430
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-05-03 23:10:55 +0300

    update gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,5 +3,31 @@
  */
 require('@nomiclabs/hardhat-waffle')
 module.exports = {
-  solidity: "0.7.3",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: "https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+      accounts: [privateKey1, privateKey2, ...]
+    }
+  },
+  solidity: {
+    version: "0.7.3",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
 };
